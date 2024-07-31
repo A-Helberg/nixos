219c1cc0622aa5765acf8f1ee218ddb42b64b716
André Helberg <helberg.andre@gmail.com>
2024-07-31 16:47:39 +0200

    get gns3 to work

--- nixos/HBD/configuration.nix
+++ nixos/HBD/configuration.nix
@@ -170,6 +170,9 @@
     qemu
     vim
     gns3-server
+    ubridge
+    virt-viewer
+    spice
 
     docker
     zlib
@@ -194,6 +197,36 @@
     enable = true;
     settings.PasswordAuthentication = false;
   };
+
+   services.gns3-server.ubridge.enable = true;
+    services.gns3-server.settings = {
+      Server.ubridge_path = pkgs.lib.mkForce "/run/wrappers/bin/ubridge";
+    };
+    users.groups.gns3 = { };
+    users.users.gns3 = {
+      group = "gns3";
+      isSystemUser = true;
+    };
+    systemd.services.gns3-server.serviceConfig = {
+      User = "gns3";
+      DynamicUser = pkgs.lib.mkForce false;
+      NoNewPrivileges = pkgs.lib.mkForce false;
+      RestrictSUIDSGID = pkgs.lib.mkForce false;
+      PrivateUsers = pkgs.lib.mkForce false;
+      DeviceAllow = [
+        "/dev/net/tun rw"
+        "/dev/net/tap rw"
+      ] ++ pkgs.lib.optionals config.virtualisation.libvirtd.enable [
+        "/dev/kvm"
+      ];
+    };
+  security.wrappers.ubridge = {
+    source = "${pkgs.ubridge}/bin/ubridge";
+    capabilities = "cap_net_admin,cap_net_raw=ep";
+    owner = "root";
+    group = "root";
+    permissions = "u+rx,g+x";
+  }; 
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
