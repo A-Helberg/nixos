69493060ca41d5c51e61723293b6bf5abcd9615b
Andre Helberg <helberg.andre@gmail.com>
2025-03-06 10:39:52 +0200

    Put back yank highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -154,13 +154,13 @@ vim.filetype.add({
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
---vim.api.nvim_create_autocmd("TextYankPost", {
---	desc = "Highlight when yanking (copying) text",
---	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
---	callback = function()
---		vim.highlight.on_yank()
---	end,
---})
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
