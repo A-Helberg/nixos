e7ed62c93402915fd9d104697f6fae18c8675091
Andre Helberg <helberg.andre@gmail.com>
2026-01-07 12:43:14 +0200

    Fix neovim colors in tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,6 +33,8 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
+-- Enable 24-bit color
+vim.opt.termguicolors = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
