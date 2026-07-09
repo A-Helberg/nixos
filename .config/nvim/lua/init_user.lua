aea485572e2ae25aefe112ff7111cd1e125a11a1
Andre Helberg <helberg.andre@gmail.com>
2026-07-09 12:47:09 +1000

    Fix metals and add tieped to nvim config

--- .config/nvim/lua/init_user.lua
+++ .config/nvim/lua/init_user.lua
@@ -1149,6 +1149,37 @@ require("lazy").setup({
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	-- { import = 'custom.plugins' },
+
+	{
+		"scalameta/nvim-metals",
+		ft = { "scala", "sbt", "java" },
+		opts = function()
+			local metals_config = require("metals").bare_config()
+			metals_config.on_attach = function(client, bufnr)
+				-- your on_attach function
+			end
+
+			return metals_config
+		end,
+		config = function(self, metals_config)
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = self.ft,
+				callback = function()
+					require("metals").initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+	},
+
+	{
+		--"A-helberg/tieped",
+		dir = "~/Projects/tieped7", -- lua/ sits at the repo root, not editors/nvim
+		main = "tieped", -- required with dir: lazy guesses the module from the dir name
+		ft = "clojure",
+		opts = { lsp = true },
+	},
 }, {
 	dev = {
 		path = "~/.local/share/nvim/nix",
@@ -1173,27 +1204,6 @@ require("lazy").setup({
 			lazy = "💤 ",
 		},
 	},
-}, {
-	"scalameta/nvim-metals",
-	ft = { "scala", "sbt", "java" },
-	opts = function()
-		local metals_config = require("metals").bare_config()
-		metals_config.on_attach = function(client, bufnr)
-			-- your on_attach function
-		end
-
-		return metals_config
-	end,
-	config = function(self, metals_config)
-		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = self.ft,
-			callback = function()
-				require("metals").initialize_or_attach(metals_config)
-			end,
-			group = nvim_metals_group,
-		})
-	end,
 })
 
 -- Floating focus buffer: visual-select lines, <leader>z to open in a centered float.
