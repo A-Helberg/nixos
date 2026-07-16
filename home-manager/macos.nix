4ffa3e7b57e1d7a2033209eb24cc777201cb7d71
Andre Helberg <helberg.andre@gmail.com>
2026-07-17 09:59:06 +1000

    Fix jetbrains clobbering my nix managed files

--- home-manager/macos.nix
+++ home-manager/macos.nix
@@ -10,4 +10,10 @@
     FLAKE = "/Users/andre/nixos";
     NH_FLAKE = "/Users/andre/nixos";
   };
+
+  # JetBrains Toolbox CLI scripts (Toolbox writes this to ~/.zprofile itself,
+  # which clobbers the home-manager managed file; keep it here instead)
+  programs.zsh.profileExtra = ''
+    export PATH="$PATH:${config.home.homeDirectory}/Library/Application Support/JetBrains/Toolbox/scripts"
+  '';
 }
